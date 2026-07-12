8a0cceb33191cf3d6ef8ae7009cc5446566b819a
Григорий Черных <grigorychernykh1@gmail.com>
2026-07-12 19:18:20 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void SayHello()
+    {
+        Console.WriteLine("hello");
+    }
+
+    static void Main(string[] args)
+    {
+        SayHello();
+    }
+}
