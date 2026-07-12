05ee0a36b67a1a36d0fdadad1709bd69ef1cf8b0
black <black@example.com>
2026-07-12 20:15:19 +0500

    Change greeting to hello everyone

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,13 @@
 ﻿class Program
 {
+<<<<<<< HEAD
+=======
+    static void SayHello()
+    {
+        Console.WriteLine("hello everyone");
+    }
+
+>>>>>>> 2fe39b7 (Change greeting to hello everyone)
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
